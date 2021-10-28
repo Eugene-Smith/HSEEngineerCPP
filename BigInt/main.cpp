48609f3db829801b5f96784b409aaf7bd588097e
Kuznetsov <kuzevgleo@gmail.com>
2021-10-28 12:09:09 +0300

    add calculation of math expressions

--- BigInt/main.cpp
+++ BigInt/main.cpp
@@ -1,27 +1,10 @@
 #include <iostream>
 #include "biginteger.h"
-
-std::string exec(const BigInteger& a, const BigInteger& b, const std::string& operation) {
-    if (operation == "+") return (a + b).toString();
-    if (operation == "-") return (a - b).toString();
-    if (operation == "*") return (a * b).toString();
-    if (operation == "/") return (a / b).toString();
-    if (operation == "%") return (a % b).toString();
-    if (operation == "<") return a < b ? "true" : "false";
-    if (operation == ">") return a > b ? "true" : "false";
-    if (operation == "<=") return a <= b ? "true" : "false";
-    if (operation == ">=") return a >= b ? "true" : "false";
-    if (operation == "==") return a == b ? "true" : "false";
-    return "Undefined operation";
-}
+#include "tools.h"
 
 int main() {
-    auto a = BigInteger();
-    auto b = BigInteger();
-    std::string operation;
-    std::cin >> a;
-    std::cin >> operation;
-    std::cin >> b;
-    std::cout << exec(a, b, operation) << std::endl;
+    std::string expression;
+    getline(std::cin, expression);
+    std::cout <<  Calculate<BigInteger>(expression) << std::endl;
     return 0;
 }
